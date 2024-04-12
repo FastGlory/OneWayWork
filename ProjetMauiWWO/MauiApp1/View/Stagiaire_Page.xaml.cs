a3d7ae65a33898e601be0cc7ab923259128f0075
Juba Redjradj <2286754@collegemv.qc.ca>
2024-04-12 14:18:19 -0400

    Évolution code (Erreur connexion DB avec FrontEnd)

--- ProjetMauiWWO/MauiApp1/View/Stagiaire_Page.xaml.cs
+++ ProjetMauiWWO/MauiApp1/View/Stagiaire_Page.xaml.cs
@@ -1,9 +1,50 @@
-namespace MauiApp1.View;
+using MauiApp1.Model;
+using MauiApp1.Service;
+using System.Collections.ObjectModel;
 
-public partial class Stagiaire_Page : ContentPage
+namespace MauiApp1.View
 {
-	public Stagiaire_Page()
-	{
-		InitializeComponent();
-	}
+    public partial class Stagiaire_Page : ContentPage
+    {
+        private readonly LocalDbService _localDbService;
+        public ObservableCollection<Stagiaire> Stagiaires { get; set; }
+
+        public Stagiaire_Page(LocalDbService dbService)
+        {
+            InitializeComponent();
+            _localDbService = dbService;
+            Stagiaires = new ObservableCollection<Stagiaire>(); 
+
+            
+            Task.Run(async () =>
+            {
+                await _localDbService.InsertDonne();
+                await LoadStagiairesAsync(); 
+            });
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadStagiairesAsync(); 
+        }
+
+        private async Task LoadStagiairesAsync()
+        {
+            try
+            {
+                var stagiaires = await _localDbService.GetStagiaire();
+                Stagiaires.Clear(); 
+                foreach (var stagiaire in stagiaires)
+                {
+                    Stagiaires.Add(stagiaire);
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                await DisplayAlert("Error", $"Impossible de récupèrer les stagiaires: {ex.Message}", "OK");
+            }
+        }
+    }
 }
