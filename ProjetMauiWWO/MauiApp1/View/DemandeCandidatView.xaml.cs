eddfe4ac4e9404256ac9869fd721ceec6d92f122
Nadine Chargui <2283110@collegemv.qc.ca>
2024-04-17 09:51:35 -0400

    mis commentaire fonctions inutile message d'Erreur

--- ProjetMauiWWO/MauiApp1/View/DemandeCandidatView.xaml.cs
+++ ProjetMauiWWO/MauiApp1/View/DemandeCandidatView.xaml.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using System.Security.Cryptography.X509Certificates;
 
 namespace MauiApp1.View;
@@ -14,7 +15,7 @@ public partial class DemandeCandidatView : ContentPage
     private async void UploadDocBtn(object sender, EventArgs e)
     {
         var typeOfdoc = new FilePickerFileType(    // On veut que le seul type de fichier que l'utilisateur prennent c'est pdf et word pour leur docs, donc on utilise un dictionnaire pour specifier quel types de fichers le user doit prendre
-            
+
             new Dictionary<DevicePlatform, IEnumerable<string>>
             {
                 {DevicePlatform.WinUI, new[] {".doc", ".docx", ".pdf"} },
@@ -33,19 +34,25 @@ public partial class DemandeCandidatView : ContentPage
             return;
         }
 
-        if (!(pickedDoc.FileName.EndsWith(".doc", StringComparison.OrdinalIgnoreCase)
-            || pickedDoc.FileName.EndsWith(".docx", StringComparison.OrdinalIgnoreCase)
-            || pickedDoc.FileName.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase)))// si le user choisi un mauvais format , il y'a un message d'avertissment
-        {
-            await DisplayAlert("Erreur", "Veuiler choisir un document de format pdf our word", "Ok");
-            return;
-        }
+        //if (!(pickedDoc.FileName.EndsWith(".doc", StringComparison.OrdinalIgnoreCase)
+        //    || pickedDoc.FileName.EndsWith(".docx", StringComparison.OrdinalIgnoreCase)
+        //    || pickedDoc.FileName.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase)))// si le user choisi un mauvais format , il y'a un message d'avertissment
+        //{
+        //    await DisplayAlert("Erreur", "Veuiler choisir un document de format pdf our word", "Ok");
+        //    return;
+        //}
 
 
         using var stream = await pickedDoc.OpenReadAsync(); // lire le doc choisis 
+
+       
+
+       
         
 
+
+
     }
-            
+
 
 }
