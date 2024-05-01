16652a29e5eeca68ad9455bfc987734aa7898a35
Nadine Chargui <2283110@collegemv.qc.ca>
2024-05-01 11:19:22 -0400

    ajt de fichier pour la candidature

---
+++ ProjetMauiWWO/MauiApp1/Model/Candidature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiApp1.Model
+{
+    [Table("Candidature")]
+    public class Candidature
+    {
+
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("idCandidature")]
+        public int Id_Candidature { get; set; }
+
+        [Column("DescriptionCandidature")]
+        public string Description_Candidature { get; set; }
+
+
+        [Column("LienCandidature")]
+        public string Lien_Candidature { get; set; }
+
+
+        [Column("DocumentCandidature")]
+        public string Document_Candidature { get; set; }
+
+
+        [Column("IsDraft")]
+        public bool Is_Draft { get; set; } = false;
+
+        [Column("IsAccepted")]
+        public bool IsAccepted { get; set; } = false;
+
+
+    }
+
+
+}
