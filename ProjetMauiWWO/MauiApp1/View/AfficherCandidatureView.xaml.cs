96d3b5d757023189f01743b8377ed9ad3d361beb
Nadine Chargui <2283110@collegemv.qc.ca>
2024-05-01 09:59:55 -0400

    ajt dossier service

---
+++ ProjetMauiWWO/MauiApp1/View/AfficherCandidatureView.xaml.cs
@@ -0,0 +1,22 @@
+namespace MauiApp1.View;
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using MauiApp1.Model;
+//using MauiApp1.Service;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+//using MauiApp1.ViewModel;
+
+public partial class AfficherCandidatureView : ContentPage
+{
+	public AfficherCandidatureView()
+	{
+
+
+		InitializeComponent();
+	}
+}
