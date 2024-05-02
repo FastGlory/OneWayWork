3c45900fe98bf105cbd8fd2ccf26d26d6f988af1
Nchargui <nnadine.chargui@gmail.com>
2024-05-02 11:09:38 -0400

    debut d'affichage pour les draft des stagiaires

--- ProjetMauiWWO/MauiApp1/View/CandidatureDraftView.xaml.cs
+++ ProjetMauiWWO/MauiApp1/View/CandidatureDraftView.xaml.cs
@@ -1,9 +1,28 @@
+using MauiApp1.Model;
+using System.Collections.ObjectModel;
+using MauiApp1.Model;
+using MauiApp1.Service;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 namespace MauiApp1.View;
 
 public partial class CandidatureDraftView : ContentPage
 {
-	public CandidatureDraftView()
-	{
-		InitializeComponent();
-	}
+
+    private readonly LocalDbService _localDbService;
+    private ObservableCollection<Candidature> _drafts;
+
+    public ObservableCollection<Candidature> Candidature { get; set; }
+
+
+    public CandidatureDraftView(LocalDbService dbService)
+    {
+
+        InitializeComponent();
+        _localDbService = dbService;
+        Candidature = new ObservableCollection<Candidature>();
+        _drafts = new ObservableCollection<Candidature>();
+        BindingContext = this;
+        
+    }
 }
